b58fbb879e1ecda7d5d6ae389f0b74e0f611a005
Salvador Fuentes Jr <fuentesjr@gmail.com>
2021-04-02 21:26:34 -0700

    Add test helper method for better testing dsl

--- test/marshmallow_test.rb
+++ test/marshmallow_test.rb
@@ -3,11 +3,7 @@
 require "test_helper"
 
 class MarshmallowTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Marshmallow::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+  test "it has a version number" do
+    refute_nil Marshmallow::VERSION
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,18 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "marshmallow"
-
 require "minitest/autorun"
+
+def test(description, &block)
+  method_name = "test_#{description.downcase.gsub(' ', '_')}"
+
+  raise "Test method (#{method_name}) already defined!!" if method_defined?(method_name)
+
+  if block_given?
+    define_method(method_name.to_sym, &block)
+  else
+    define_method(method_name.to_sym) do
+      flunk "TEST #{method_name} is not defined"
+    end
+  end
+end
